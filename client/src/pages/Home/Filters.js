6bca3d2b0f8efdf56ea4a06f526aa1a9c93e1866
Belle <belleliu27@gmail.com>
2024-10-03 18:29:44 +1000

    add filtering options

---
+++ client/src/pages/Home/Filters.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const categories = [
+  {
+    name: "Electronics",
+    value: "electronics",
+  },
+  {
+    name: "Home",
+    value: "home",
+  },
+  {
+    name: "Fashion",
+    value: "fashion",
+  },
+  {
+    name: "Sports",
+    value: "sports",
+  },
+  {
+    name: "Books",
+    value: "books",
+  },
+];
+
+const ages = [
+  {
+    name: "0-2 years old",
+    value: "0-2",
+  },
+  {
+    name: "3-5 years old",
+    value: "3-5",
+  },
+  {
+    name: "6-8 years old",
+    value: "6-8",
+  },
+  {
+    name: "9-12 years old",
+    value: "9-12",
+  },
+  {
+    name: "13+ years old",
+    value: "12-20",
+  },
+];
